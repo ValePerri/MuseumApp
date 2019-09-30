310a5db51d145afc7bedf1f8d6fecc6c3cf3f47c
ValePerri <v.perrini@studenti.unisa.it>
2019-09-30 10:59:39 +0200

    login e sognup

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -3,11 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  styleUrls: ['../home.page.scss', './login.component.scss'],
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  logo: string
+
+  constructor() {
+
+   }
 
   ngOnInit() {}
 
--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss'],
+  styleUrls: ['../home.page.scss', './signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
 
