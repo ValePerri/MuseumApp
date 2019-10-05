b870e3057ef954f8189174ac2c1536cac4daca5f
ValePerri <v.perrini@studenti.unisa.it>
2019-10-05 09:30:58 +0200

    about us e hamburger menu in progress

--- src/app/menu/scan/scan.component.ts
+++ src/app/menu/scan/scan.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { ControlContainer } from '@angular/forms';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-scan',
   templateUrl: './scan.component.html',
   styleUrls: ['../menu.page.scss','./scan.component.scss'],
 })
-export class ScanComponent implements OnInit {
+export class ScanComponent  {
 
-  constructor() { }
+  constructor(private menu: MenuController) { }
 
-  ngOnInit() {}
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
 
+  openEnd() {
+    this.menu.open('end');
+  }
+  
 }
